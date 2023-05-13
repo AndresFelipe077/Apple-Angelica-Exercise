6943831e8d3ae300de41f0e658e8bc3358463bc8
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-05-13 09:45:02 -0500

    Se incluyen enlaces

--- img/zcode/app.js
+++ img/zcode/app.js
@@ -3,6 +3,11 @@ const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
 
+  autoplay: {
+    delay: 4000, // Tiempo de espera entre cada cambio de slide en milisegundos
+    disableOnInteraction: false, // Permite que el autoplay continúe incluso cuando el usuario interactúa con el Swiper
+  },
+
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
